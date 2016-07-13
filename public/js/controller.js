6670161a43bc7e9113f9df104aaf51d026438504
chocko <chockowhim@gmail.com>
2016-07-12 19:49:01 -0500

    delete + buscar

--- public/js/controller.js
+++ public/js/controller.js
@@ -11,6 +11,27 @@ $scope.archivos = data;
 })
 .error(function (err) {
 
-})
+});
+// delete a todo after checking it
+$scope.deleteTodo = function(id) {
+$http.delete('/archivos/delete/' + id)
+    .success(function(data) {
+        $scope.refresh();
+          console.log(data)  
+          })
+  .error(function(data) {
+        console.log('Error: ' + data);
+          });
+};
+
+//refresh
+
+$scope.refresh = function(){
+    $http.get('/archivos/list')
+          .success(function(data){
+               $scope.archivos = data;
+
+          });
+};
 
 }])
